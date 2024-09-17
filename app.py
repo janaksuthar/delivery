ac2b1cab9ab6609c2a2a2ff70a75363402a3a1de
janaksuthar <42261530+janaksuthar@users.noreply.github.com>
2024-09-18 00:02:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pickle
 
 # Load the trained model
-filename = 'knn_model.sav'
+filename = 'https://github.com/janaksuthar/delivery/blob/main/knn_model.sav'
 loaded_model = pickle.load(open(filename, 'rb'))
 
 # Define the prediction function
